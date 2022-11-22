6c2e41dc1728651f5491ce6cdeae25baf7eb4b9f
Yafira <yafira.martinez@lc.cuny.edu>
2022-11-21 20:33:28 -0500

    fixed issue with images not showing

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import './App.css'
 function App() {
 	const [userPrompt, setPrompt] = useState('')
 	const [result, setResult] = useState('')
-	const api_key = process.env.REACT_APP_OPENAI_API_KEY
+	const api_key = process.env.REACT_APP_OPENAI_KEY
 	const configuration = new Configuration({
 		apiKey: api_key,
 	})
 	const openai = new OpenAIApi(configuration)
 	console.log(api_key)
+
 	const generateImage = async () => {
-		const imageParameters = {
+		const response = await openai.createImage({
 			prompt: userPrompt,
 			n: 1,
 			size: '256x256',
-		}
-		const response = await openai.createImage(imageParameters)
+		})
 		setResult(response.data.data[0].url)
 	}
 
