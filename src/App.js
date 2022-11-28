365d185f5ee840ef852d8101ceb757dcbb43e3ac
Yafira <yafira.martinez@lc.cuny.edu>
2022-11-27 20:28:21 -0500

    added media queries + changed class to className

--- src/App.js
+++ src/App.js
@@ -32,16 +32,16 @@ function App() {
 			{loading ? (
 				<>
 					<h2>Generating in effect...</h2>
-					<div class='spinner'>
+					<div className='spinner'>
 						<svg
-							class='spinner'
+							className='spinner'
 							width='65px'
 							height='65px'
 							viewBox='0 0 66 66'
 							xmlns='http://www.w3.org/2000/svg'
 						>
 							<circle
-								class='path'
+								className='path'
 								fill='none'
 								stroke-width='6'
 								stroke-linecap='round'
