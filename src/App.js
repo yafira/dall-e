cd1e29f060824c27abcff61f8bfc8cc670f9558e
Yafira <yafira.martinez@lc.cuny.edu>
2022-12-07 21:53:55 -0500

    moved media queries to separate file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import './style.css'
 import './loader.scss'
+import './queries.css'
 
 function App() {
 	const [userPrompt, setPrompt] = useState('')
